b8c9f46079114ad4caf65ebef915ab822fc6cecb
vipro999 <son92qb@gmail.com>
2025-04-06 09:41:00 +0700

    Update Telegram Media Downloader.user.js

--- Telegram Media Downloader.user.js
+++ Telegram Media Downloader.user.js
@@ -1,26 +1,3 @@
-// ==UserScript==
-// @name         Telegram Media Downloader
-// @name:en      Telegram Media Downloader
-// @name:zh-CN   Telegram 受限图片视频下载器
-// @name:zh-TW   Telegram 受限圖片影片下載器
-// @name:ru      Telegram: загрузчик медиафайлов
-// @version      1.206
-// @namespace    https://github.com/Neet-Nestor/Telegram-Media-Downloader
-// @description  Download images, GIFs, videos, and voice messages on the Telegram webapp from private channels that disable downloading and restrict saving content
-// @description:en  Download images, GIFs, videos, and voice messages on the Telegram webapp from private channels that disable downloading and restrict saving content
-// @description:ru Загружайте изображения, GIF-файлы, видео и голосовые сообщения в веб-приложении Telegram из частных каналов, которые отключили загрузку и ограничили сохранение контента
-// @description:zh-CN 从禁止下载的Telegram频道中下载图片、视频及语音消息
-// @description:zh-TW 從禁止下載的 Telegram 頻道中下載圖片、影片及語音訊息
-// @author       Nestor Qin
-// @license      GNU GPLv3
-// @website      https://github.com/Neet-Nestor/Telegram-Media-Downloader
-// @match        https://web.telegram.org/*
-// @match        https://webk.telegram.org/*
-// @match        https://webz.telegram.org/*
-// @icon         https://img.icons8.com/color/452/telegram-app--v5.png
-// @downloadURL https://update.greasyfork.org/scripts/446342/Telegram%20Media%20Downloader.user.js
-// @updateURL https://update.greasyfork.org/scripts/446342/Telegram%20Media%20Downloader.meta.js
-// ==/UserScript==
 
 
 (function () {
